b2a0941848dbe9a81fc44fad37bfd96977dae6fa
Ranjeev Mahtani <ranjeevmahtani@gmail.com>
2015-08-28 16:18:01 -0700

    add messages via toasts for issues encountered in fetchmoviestask

--- app/src/main/java/com/example/android/popularmovies/FetchMoviesTask.java
+++ app/src/main/java/com/example/android/popularmovies/FetchMoviesTask.java
@@ -15,9 +15,14 @@ import java.util.Arrays;
  * Created by ranjeevmahtani on 8/14/15.
  */
 
-public class FetchMoviesTask extends AsyncTask<String, Void, Void> {
+public class FetchMoviesTask extends AsyncTask<String, Void, Integer> {
 
     private final String LOG_TAG = FetchMoviesTask.class.getSimpleName();
+
+    private final int EVERYTHING_IS_GOOD_CODE = 100;
+    private final int NO_INTERNET_CONNECTION_CODE = 200;
+    private final int NO_MOVIES_RETURNED_FROM_SERVER_CODE = 300;
+    private final int JSON_ERROR_CODE = 400;
 
     private Context mContext;
     private MoviePosterAdapter mMoviePosterAdapter;
@@ -30,7 +35,7 @@ public class FetchMoviesTask extends AsyncTask<String, Void, Void> {
     }
 
     @Override
-    protected Void doInBackground(String... sortOption) {
+    protected Integer doInBackground(String... sortOption) {
 
         URL queryURL = Utility.getDiscoveryQueryUrl(mContext, sortOption[0]);
 
@@ -44,31 +49,50 @@ public class FetchMoviesTask extends AsyncTask<String, Void, Void> {
                 if (movies != null && movies.length >0) {
                     mMovieArrayList.clear();
                     mMovieArrayList.addAll(Arrays.asList(movies));
+                    return EVERYTHING_IS_GOOD_CODE;
+                } else {
+                    return NO_MOVIES_RETURNED_FROM_SERVER_CODE;
                 }
 
-                return null;
             } catch (JSONException e) {
                 Log.e(LOG_TAG, e.getMessage(), e);
                 e.printStackTrace();
-                return null;
+                return JSON_ERROR_CODE;
             }
         } else {
-            Log.d(LOG_TAG, "Internet connection not available.");
-            return null;
+            return NO_INTERNET_CONNECTION_CODE;
         }
     }
 
     @Override
-    protected void onPostExecute(Void result) {
+    protected void onPostExecute(Integer resultCode) {
 
-        if (mMovieArrayList != null) {
+        if (resultCode == EVERYTHING_IS_GOOD_CODE && mMovieArrayList != null) {
             mMoviePosterAdapter.clear();
             mMoviePosterAdapter.addAll(mMovieArrayList);
             mMoviePosterAdapter.notifyDataSetChanged();
         }
         else {
-            String offlineMessage = "You aren't connected to the interwebs. Try again when you are connected.";
-            Toast toast = Toast.makeText(mContext, offlineMessage, Toast.LENGTH_SHORT);
+            final String NO_INTERNET_CONNECTION_MESSAGE = "Can you has interwebs?";
+            final String NO_MOVIES_FOUND_MESSAGE = "Couldn't find any movies :(";
+            final String JSON_ERROR_MESSAGE = "Server be sending us some bogusness...";
+            final String UNKNOWN_ERROR_MESSAGE = "Someone farted";
+            Toast toast = Toast.makeText(mContext, UNKNOWN_ERROR_MESSAGE, Toast.LENGTH_SHORT);
+
+            switch (resultCode) {
+                case NO_INTERNET_CONNECTION_CODE: {
+                    toast.setText(NO_INTERNET_CONNECTION_MESSAGE);
+                    break;
+                }
+                case JSON_ERROR_CODE: {
+                    toast.setText(JSON_ERROR_MESSAGE);
+                    break;
+                }
+                case NO_MOVIES_RETURNED_FROM_SERVER_CODE: {
+                    toast.setText(NO_MOVIES_FOUND_MESSAGE);
+                    break;
+                }
+            }
             toast.show();
         }
     }
